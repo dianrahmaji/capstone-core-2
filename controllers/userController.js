0be7d7e8886068c84c6af014535f164328454dae
Dian Rahmaji <dianrahmaji@gmail.com>
2022-05-23 10:39:21 +0700

    omit admin from getAllUsers (#18)

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,7 +72,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route GET /api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({})
+  const users = await User.find({ isAdmin: false })
   res.json(users)
 })
 
