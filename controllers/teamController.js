67a042058746d6f4e14d45bf88e256d849f18e47
Dian Rahmaji <dianrahmaji@gmail.com>
2022-07-13 10:33:44 +0700

    KMNCORE-30 Added GetTeams and GetTeamById (#46)
    * chore: added getTeamById controller
    
    * chore: added getTeams controller
    
    * feat: registered getTeams and getTeamById to teamRoute

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -34,4 +34,26 @@ const approveTeam = asyncHandler(async (req, res) => {
   res.status(200).json(approvedTeam)
 })
 
-export { createTeam, approveTeam }
+// @desc Get All Team
+// @route GET /api/team
+// @access Private/Admin
+const getTeams = asyncHandler(async (req, res) => {
+  const teams = await Team.find({})
+  res.status(200).json(teams)
+})
+
+// @desc Get Team by Id
+// @route GET /api/team/:id
+// @access Private/User
+const getTeamById = asyncHandler(async (req, res) => {
+  const team = await Team.findById(req.params.id)
+
+  if (!team) {
+    res.status(404)
+    throw new Error('Team not found')
+  }
+
+  res.status(200).json(team)
+})
+
+export { createTeam, approveTeam, getTeamById, getTeams }
--- routes/teamRoute.js
+++ routes/teamRoute.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { createTeam, approveTeam } from '../controllers/teamController.js'
+import {
+  createTeam,
+  approveTeam,
+  getTeamById,
+  getTeams
+} from '../controllers/teamController.js'
 import { admin, protect } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(protect, createTeam)
+router.route('/').post(protect, createTeam).get(protect, admin, getTeams)
+router.route('/:id').get(protect, getTeamById)
 router.route('/:id/approve').put(protect, admin, approveTeam)
 
 export default router
