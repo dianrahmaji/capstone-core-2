65503678121ead0a85d3e1360ab29d0ab1bc9469
Dian Rahmaji <dianrahmaji@gmail.com>
2022-07-21 22:42:54 +0700

    KMNCORE-21 Added User Data Seed (#70)
    * feat: added seedData function
    
    * feat: added user.json
    
    * feat: added user seed

--- data/seeder.js
+++ data/seeder.js
@@ -10,6 +10,8 @@ import Repository from '../models/repositoryModel.js'
 import Team from '../models/teamModel.js'
 import User from '../models/userModel.js'
 
+import users from './user.json'
+
 dotenv.config()
 
 connectDB()
@@ -33,6 +35,29 @@ const destroyData = async () => {
   }
 }
 
+const seedData = async () => {
+  try {
+    await Promise.all([
+      Document.deleteMany({}),
+      Folder.deleteMany({}),
+      Message.deleteMany({}),
+      Repository.deleteMany({}),
+      Team.deleteMany({}),
+      User.deleteMany({})
+    ])
+
+    await User.create(users)
+
+    console.log('Data Imported!'.green)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red)
+    process.exit(1)
+  }
+}
+
 if (process.argv[2] === '-d') {
   destroyData()
+} else {
+  seedData()
 }
